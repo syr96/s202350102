c0297da3656dae762ea3f45a96fa0da2e899f273
Yu-Rim Seok <96058798+syr96@users.noreply.github.com>
2023-10-30 15:04:32 +0900

    최초 등록

---
+++ S202350102/src/main/java/com/oracle/S202350102/controller/HbController.java
@@ -0,0 +1,15 @@
+package com.oracle.S202350102.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HbController {
+	
+	
+}
